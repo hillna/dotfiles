00857a691a0e86fdab31390b406bb96f2c4993c1
Nick Hill <nickh@olark.com>
2015-12-21 00:06:20 -0500

    forgot to add file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -d $HOME/.oh-my-zsh ]; then
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi 
+
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+    mkdir -p $HOME/.vim/bundle
+    git clone clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
+
