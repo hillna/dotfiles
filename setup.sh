c65b256c343d080afca9a67af0b1512dddd7bc9f
Nick Hill <nickh@olark.com>
2016-01-25 11:00:48 -0500

    larger bspwm borders, Xresource colors only for rofi, install vim plugins in bootstrap

--- setup.sh
+++ setup.sh
@@ -7,5 +7,6 @@ fi
 if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
     mkdir -p $HOME/.vim/bundle
     git clone clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
 fi
 
