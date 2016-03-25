c23dda5e409e38a8292ab49b0e771713ad9be9fb
Nick Hill <nickh@olark.com>
2016-03-25 01:07:35 -0400

    update bootstrap

--- setup.sh
+++ setup.sh
@@ -2,11 +2,15 @@
 
 if [ ! -d $HOME/.oh-my-zsh ]; then
     sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-fi 
+fi
 
 if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
     mkdir -p $HOME/.vim/bundle
     git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
+fi
+
+if [! -d $HOME/.local/lib/python2.7/site-packages/powerline ]; then
+    pip install --user powerline-status
 fi
 
