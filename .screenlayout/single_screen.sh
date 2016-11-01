60a73513164c673b3f7549e89fc5faa7bc166603
Nick Hill <nickh@olark.com>
2016-11-01 17:42:29 -0400

    scale screen on mac

--- .screenlayout/single_screen.sh
+++ .screenlayout/single_screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --mode 1680x1050 --pos 0x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off
+xrandr --output eDP1 --scale .75x.75
