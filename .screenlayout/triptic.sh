f33bcb467de1f03dc07a895b053e0e31ce328428
Nick Hill <nickh@olark.com>
2018-08-21 13:04:39 -0400

    compositing updates, xprofile updates

--- .screenlayout/triptic.sh
+++ .screenlayout/triptic.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate left --output DisplayPort-0 --primary --mode 1920x1080 --pos 1080x424 --rotate normal --output HDMI-A-1 --off --output HDMI-A-0 --mode 1920x1080 --pos 3000x0 --rotate right
+xrandr --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate left --output DisplayPort-0 --primary --mode 1920x1080 --pos 1080x432 --rotate normal --output HDMI-A-1 --off --output HDMI-A-0 --mode 1920x1080 --pos 3000x0 --rotate right
