33277c53f602d2d8d24b665d263e710ac08b62cd
Nick Hill <nickh@olark.com>
2018-08-24 11:53:22 -0400

    update updates script

--- .config/polybar/updates.sh
+++ .config/polybar/updates.sh
@@ -5,5 +5,7 @@ aur_updates="$(checkupdates-aur |wc -l)"
 
 if [ ${arch_updates} -gt 0 ] || [ ${aur_updates} -gt 0 ]; then
     echo -e " ${arch_updates}(${aur_updates})"
+else
+    echo -e ""
 fi
 
