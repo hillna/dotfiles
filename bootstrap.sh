e97d31a010417693b19fc26238cb17fe0a38c4c8
Nick Hill <nickh@olark.com>
2018-09-03 21:02:42 -0400

    cleanup bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,6 @@ if [ ! -d $HOME/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
-#if [ ! -d $HOME/.oh-my-zsh ]; then
-#    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-#fi
-
 if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
     mkdir -p $HOME/.vim/bundle
     git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
@@ -20,6 +16,4 @@ fi
 if [ ! -d $HOME/.local/lib/python2.7/site-packages/powerline ]; then
     pip2 install --user powerline-status
 fi
-
-#bin/dfm install
 
