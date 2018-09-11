431d5852852f2641cc49d81cd7122b82ce3a7a3f
Nick Hill <nickh@olark.com>
2018-09-11 09:16:06 -0400

    nvim config

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,12 @@ if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
     vim +PluginInstall +qall
 fi
 
+if [ ! -d $HOME/.config/nvim/bundle/Vundle.vim ]; then
+    mkdir -p $HOME/.config/nvim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.config/nvim/bundle/Vundle.vim
+    nvim +PluginInstall +qall
+fi
+
 if [ ! -d $HOME/.local/lib/python2.7/site-packages/powerline ]; then
     pip2 install --user powerline-status
 fi
