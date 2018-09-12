cb236fce48356dd5ccbbbb05d5026aff7937b627
Nick Hill <nickh@olark.com>
2018-09-12 14:12:03 -0400

    more bootstrap updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,15 +9,11 @@ if [ ! -d $HOME/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
-vundledirs=($HOME/.vim/bundle $HOME/.config/nvim/bundle)
-for dir in $vundledirs; do
-    if [ ! -d $dir/Vundle.vim ]; then
-        mkdir -p $dir
-        git clone https://github.com/VundleVim/Vundle.vim.git $dir/Vundle.vim
-    fi
-done
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+    mkdir -p $HOME/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
 vim +PluginInstall +qall
-nvim +PluginInstall +qall
 
 if [ ! -d $HOME/.local/lib/python2.7/site-packages/powerline ]; then
     pip2 install --user powerline-status
