dda2e2970df3bbb0feb836abc806d77f7bf9f1f8
Nick Hill <nickh@olark.com>
2018-09-12 13:53:26 -0400

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,14 @@ if [ ! -d $HOME/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
-vundledirs=($HOME/.vim/bundle $HOME/.config/nvim/bundle)
+vundledirs={$HOME/.vim/bundle,$HOME/.config/nvim/bundle}
 for dir in $vundledirs; do
     if [ ! -d $dir/Vundle.vim ]; then
         mkdir -p $dir
         git clone https://github.com/VundleVim/Vundle.vim.git $dir/Vundle.vim
-        vim +PluginInstall +qall
     fi
+    vim +PluginInstall +qall
+    nvim +PluginInstall +qall
 done
 
 if [ ! -d $HOME/.local/lib/python2.7/site-packages/powerline ]; then
