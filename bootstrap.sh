a19b926b89f88598c9214763d25455f65bc9fee0
Nick Hill <nickh@olark.com>
2018-09-12 14:05:05 -0400

    updated dfm install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Initiating bootstrap"
+
 cd $HOME/.dotfiles
 git submodule init && git submodule update
 
@@ -7,17 +9,19 @@ if [ ! -d $HOME/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
-vundledirs={$HOME/.vim/bundle,$HOME/.config/nvim/bundle}
+vundledirs=($HOME/.vim/bundle $HOME/.config/nvim/bundle)
 for dir in $vundledirs; do
     if [ ! -d $dir/Vundle.vim ]; then
         mkdir -p $dir
         git clone https://github.com/VundleVim/Vundle.vim.git $dir/Vundle.vim
     fi
-    vim +PluginInstall +qall
-    nvim +PluginInstall +qall
 done
+vim +PluginInstall +qall
+nvim +PluginInstall +qall
 
 if [ ! -d $HOME/.local/lib/python2.7/site-packages/powerline ]; then
     pip2 install --user powerline-status
 fi
+
+echo "Bootstrap completed!"
 
