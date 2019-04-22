8b3fb9a6afb9e71c1d0c32cd8f87f72f68d2fe4d
Nick Hill <nickh@olark.com>
2019-04-22 12:17:03 -0400

    update submodules for deoplete-jedi in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,10 @@ if [ ! -d $HOME/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
+if [ ! -d $HOME/.local/lib/python2.7/site-packages/powerline ]; then
+    pip2 install --user powerline-status
+fi
+
 if [ ! -f $HOME/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -16,8 +20,9 @@ fi
 vim +'PlugInstall --sync' +qa
 nvim +'PlugInstall --sync' +qa
 
-if [ ! -d $HOME/.local/lib/python2.7/site-packages/powerline ]; then
-    pip2 install --user powerline-status
+if [ ! -d $HOME/.vim/plugged/deoplete-jedi/rplugin/python3/deoplete/vendored/jedi ]; then
+    cd $HOME/.vim/plugged/deoplete-jedi && \
+    git submodule update --init
 fi
 
 echo "Bootstrap completed!"
