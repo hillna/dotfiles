ad25185377821caa61da6963fb951629d31b4e78
Nick Hill <nickh@olark.com>
2019-05-28 16:52:39 -0400

    jack stuff.

--- .jack/jack-post-start.sh
+++ .jack/jack-post-start.sh
@@ -6,4 +6,6 @@ pacmd set-default-source jack_in
 alsa_out -j "Modi Playback" -d hw:1,1 &
 #alsa_in -j "Onboard In" -d hw:2,0 &
 
+sleep 5s
+
 #a2jmidid -e &
--- .jack/jack-post-stop.sh
+++ .jack/jack-post-stop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+autoclose=(claudia ladishd)
+for proc in ${autoclose[@]}; do
+  if pgrep $proc; then
+    pkill $proc
+    sleep 1s
+  fi
+done
+
 pacmd suspend false
 
 pacmd set-default-sink 0
