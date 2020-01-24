c79e0c44ee0a4ec38f96ab5f96c15b0c5f32dbbc
Nick Hill <nickh@olark.com>
2020-01-24 16:44:12 -0500

    Update screen layout

--- .screenlayout/triptic.sh
+++ .screenlayout/triptic.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1080x420 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-A-0 --mode 1920x1080 --pos 3000x0 --rotate right --output HDMI-A-1 --off
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1080x420 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-A-0 --mode 1920x1080 --pos 3000x420 --rotate normal --output HDMI-A-1 --off
