4589b51c9eb4c15b7b8800c4d71033ce1a1d64de
Nick Hill <nickh@olark.com>
2020-01-24 22:01:28 -0500

    External rules for godot

--- .config/bspwm/external-rules.sh
+++ .config/bspwm/external-rules.sh
@@ -4,6 +4,7 @@ wid=$1
 class=$2
 instance=$3
 
+
 if [ "$class" = Chromium ]; then
   title=$(xtitle "$wid")
   case "$title" in
@@ -12,3 +13,19 @@ if [ "$class" = Chromium ]; then
       ;;
   esac
 fi
+
+if [ "$instance" = Godot_Engine ]; then
+  sleep 0.15s
+  title=$(xtitle "$wid")
+  case "$title" in
+    Godot)
+      echo "state = tiled"
+      ;;
+    *)
+      echo "state = floating"
+      ;;
+  esac
+fi
+
+# Uncomment below to test
+# echo -ne "id $wid\nclass $class\ninst $instance\ntitle $title\n" >> /tmp/extout.txt
