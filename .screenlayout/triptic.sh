6231dc714f3db18541633ca67dea8a3b7b32f49b
hillna <nick@nickh.info>
2022-06-23 23:46:24 -0400

    Update screen layout and hotkeys

--- .screenlayout/triptic.sh
+++ .screenlayout/triptic.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 2561x483 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 0x483 --rotate normal --output HDMI-A-0 --mode 2560x1440 --pos 5121x0 --rotate right --output HDMI-A-1 --off
+xrandr \
+  --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x550 --rotate normal \
+  --output DisplayPort-1 --mode 2560x1440 --pos 0x550 --rotate normal \
+  --output HDMI-A-0 --mode 2560x1440 --pos 5120x0 --rotate right \
+  --output HDMI-A-1 --off
