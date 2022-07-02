6dfb5e2188954bf375a63f3b06fd9dc8cff68568
hillna <nick@nickh.info>
2022-07-02 15:33:42 -0400

    Use paru for aur updates

--- .config/polybar/updates.sh
+++ .config/polybar/updates.sh
@@ -2,13 +2,16 @@
 
 if [ -f /usr/bin/checkupdates ]; then
   arch_updates="$(checkupdates | wc -l)"
+fi
+if [ -f /usr/bin/paru ]; then
 #  aur_updates="$(checkupdates-aur |wc -l)"
-  aur_updates="$(minq-checkupdates-aur |wc -l)"
+#  aur_updates="$(minq-checkupdates-aur |wc -l)"
+  aur_updates="$(paru -Qua |wc -l)"
+fi
 
-  if [ ${arch_updates} -gt 0 ] || [ ${aur_updates} -gt 0 ]; then
-    echo -e " ${arch_updates}(${aur_updates})"
-  else
-    echo -e ""
-  fi
+if [ ${arch_updates} -gt 0 ] || [ ${aur_updates} -gt 0 ]; then
+  echo -e " ${arch_updates}(${aur_updates})"
+else
+  echo -e ""
 fi
 
