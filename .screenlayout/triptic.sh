db059b487fb18918ac9877d5136d29f47f041c4a
hillna <nick@nickh.info>
2022-09-05 20:21:47 -0400

    Update screen refresh rates

--- .screenlayout/triptic.sh
+++ .screenlayout/triptic.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 xrandr \
-  --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x550 --rotate normal \
-  --output DisplayPort-1 --mode 2560x1440 --pos 0x550 --rotate normal \
+  --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x550 --rate 165 --rotate normal \
+  --output DisplayPort-1 --mode 2560x1440 --pos 0x550 --rate 165 --rotate normal \
   --output HDMI-A-0 --mode 2560x1440 --pos 5120x0 --rotate right \
   --output HDMI-A-1 --off
+
