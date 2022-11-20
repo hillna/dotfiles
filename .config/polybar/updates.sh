8c1378d300523118d4f894a98f3e4f0a7a714126
hillna <nick@nickh.info>
2022-11-20 01:03:55 -0500

    Update some superslicer files

--- .config/polybar/updates.sh
+++ .config/polybar/updates.sh
@@ -2,10 +2,14 @@
 
 if [ -f /usr/bin/checkupdates ]; then
   arch_updates="$(checkupdates | wc -l)"
+else
+  arch_updates="0"
 fi
 
 if [ -f /usr/bin/paru ]; then
   aur_updates="$(paru -Qua |wc -l)"
+else
+  aur_updates="0"
 fi
 
 if [ ${arch_updates} -gt 0 ] || [ ${aur_updates} -gt 0 ]; then
