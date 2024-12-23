6ec82e594cad98345f34b509a83be522b439a6a5
Nick Hill <nick@nickh.info>
2024-12-23 12:03:24 -0500

    Updated nvim theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,18 @@
 return {
   -- add colorschemes
+  { "AlexvZyl/nordic.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "ellisonleao/gruvbox.nvim" },
   { "folke/tokyonight.nvim" },
+  { "sainnhe/everforest" },
+  { "shaunsingh/nord.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "gruvbox",
-      style = "moon",
+      colorscheme = "catppuccin-macchiato",
+      --style = "moon",
     },
   },
 }
