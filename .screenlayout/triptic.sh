bb983ef17088ea9d18fbe63c5e0436020cf4ac11
Nick Hill <nick@nickh.info>
2025-01-21 00:36:35 -0500

    Update screen layout

--- .screenlayout/triptic.sh
+++ .screenlayout/triptic.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 xrandr \
-  --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x550 --rate 165 --rotate normal \
+  --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x550 --rate 360 --rotate normal \
   --output DisplayPort-1 --mode 2560x1440 --pos 0x550 --rate 165 --rotate normal \
-  --output HDMI-A-0 --mode 2560x1440 --pos 5120x0 --rotate right \
+  --output HDMI-A-0 --mode 2560x1440 --pos 5120x0 --rate 165 --rotate right \
   --output HDMI-A-1 --off
-
