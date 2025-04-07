02dd34f1606f51e3184334df34b3c02db1e884b6
Nick Hill <nick@nickh.info>
2025-04-06 22:37:28 -0400

    new screen layout

--- .screenlayout/single_left.sh
+++ .screenlayout/single_left.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 xrandr \
   --output DisplayPort-0 --off \
-  --output DisplayPort-1 --mode 2560x1440 --pos 0x0 --rate 165 --rotate normal \
+  --output DisplayPort-1 --off \
+  --output DisplayPort-2 --mode 2560x1440 --pos 0x0 --rate 165 --rotate normal \
+  --output DisplayPort-3 --off \
+  --output DisplayPort-4 --off \
   --output HDMI-A-0 --off \
-  --output HDMI-A-1 --off
-
+  --output HDMI-A-1-1 --off
--- .screenlayout/triptic.sh
+++ .screenlayout/triptic.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 xrandr \
-  --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x550 --rate 360 --rotate normal \
-  --output DisplayPort-1 --mode 2560x1440 --pos 0x550 --rate 165 --rotate normal \
+  --output DisplayPort-0 --off \
+  --output DisplayPort-1 --primary --mode 2560x1440 --pos 2560x550 --rate 360 --rotate normal \
+  --output DisplayPort-2 --mode 2560x1440 --pos 0x550 --rate 165 --rotate normal \
+  --output DisplayPort-3 --off \
+  --output DisplayPort-4 --off \
   --output HDMI-A-0 --mode 2560x1440 --pos 5120x0 --rate 165 --rotate right \
-  --output HDMI-A-1 --off
+  --output HDMI-A-1-1 --off
