2722ea03a38624186dafafe60be697ddaff023e5
Nick Hill <nick@nickh.info>
2025-04-24 09:31:31 -0400

    Update bootstrap and shellrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,15 +10,15 @@ if [ ! -d $HOME/.zprezto ]; then
 fi
 
 # Powerline & modules
-if [ $(uname | grep Darwin) ]; then
-  if [ ! -d /usr/local/lib/python3.9/site-packages/powerline ]; then
-    pip3 install powerline-status
-  fi
-else
-  if [ ! -d $HOME/.local/lib/python3.8/site-packages/powerline ]; then
-    pip3 install --user powerline-status
-  fi
-fi
+#if [ $(uname | grep Darwin) ]; then
+#  if [ ! -d /usr/local/lib/python3.9/site-packages/powerline ]; then
+#    pip3 install powerline-status
+#  fi
+#else
+#  if [ ! -d $HOME/.local/lib/python3.8/site-packages/powerline ]; then
+#    pip3 install --user powerline-status
+#  fi
+#fi
 
 # Original Powerkube: https://github.com/zcmarine/powerkube
 # Fixed import: https://github.com/rarguelloF/powerkube/tree/fix-kubernetes-import
@@ -27,10 +27,10 @@ fi
 #fi
 
 # vim plugins
-if [ ! -f $HOME/.vim/autoload/plug.vim ]; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
+#if [ ! -f $HOME/.vim/autoload/plug.vim ]; then
+#  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#fi
 #vim +'PlugInstall --sync' +qa
 #nvim +'PlugInstall --sync' +qa
 #nvim +'UpdateRemotePlugins' +qa
