c9ec3ffdbb1d6052b52e546587cde1aae310b34f
Nick Hill <nick@nickh.info>
2025-05-28 12:18:04 -0400

    Update bspwmrc

--- .jack/stop_jack.sh
+++ .jack/stop_jack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Unload Pulse Audio JACK modules
-for module in $(pactl list modules | grep -B 1 "Name: module-jack" |grep Module | sed 's/[^0-9]//g'); do
+for module in $(pactl list modules | grep -B 1 "Name: module-jack" | grep Module | sed 's/[^0-9]//g'); do
   pactl unload-module $module
 done
 
@@ -17,3 +17,4 @@ pacmd suspend false
 pacmd set-default-sink 0
 pacmd set-default-source 0
 
+#pactl set-card-profile "USB Modi Device" iec958-output
